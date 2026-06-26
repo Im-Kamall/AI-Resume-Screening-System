bc7ef166059944d5145b4e408e89624a00fa553d
Kamal Solanki <solankikamal754@gmail.com>
2026-06-27 00:21:30 +0530

    Fix NLTK resources for Streamlit deployment

--- src/text_cleaner.py
+++ src/text_cleaner.py
@@ -1,18 +1,32 @@
 import re
 import nltk
 
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
+
+try:
+    nltk.data.find("corpora/omw-1.4")
+except LookupError:
+    nltk.download("omw-1.4")
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
 
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
+
 def clean_text(text):
-
     text = str(text).lower()
-
     text = re.sub(r"http\S+", "", text)
-
     text = re.sub(r"[^a-zA-Z ]", " ", text)
 
     words = text.split()
